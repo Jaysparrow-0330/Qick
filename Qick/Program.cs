c84778fe115c80c28683ba0709bc09e12ca84478
Jaysparrow-0330 <jack3460@gmail.com>
2022-10-13 21:47:12 +0700

    sửa swagger

--- Qick/Program.cs
+++ Qick/Program.cs
@@ -41,12 +41,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
    
-}
+
 
 app.UseCors("default");
 app.UseHttpsRedirection();
