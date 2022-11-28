fcc749683ae08d82d81e11f4b09dda604642c7a6
Jaysparrow-0330 <jack3460@gmail.com>
2022-11-28 14:30:52 +0700

    an commit

--- Qick/Dto/Responses/TakingTestResponse.cs
+++ Qick/Dto/Responses/TakingTestResponse.cs
@@ -3,7 +3,7 @@
     public class TakingTestResponse
     {
         public int Id { get; set; }
-        public string QuizName { get; set; }
+        public string TestName { get; set; }
         public string? BannerUrl { get; set; }
         public ICollection<QuestionResponse> questions { get; set; }
     }
--- Qick/Dto/Responses/TestDetailResponse.cs
+++ Qick/Dto/Responses/TestDetailResponse.cs
@@ -3,7 +3,7 @@
     public class TestDetailResponse
     {
         public int Id { get; set; }
-        public string? QuizName { get; set; }
+        public string? TestName { get; set; }
         public string? BannerUrl { get; set; }
         public int? TotalQuestion { get; set; }
         public string? Introduction { get; set; }
