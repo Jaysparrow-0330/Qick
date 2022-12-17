1613cf676365b14517495a8a6a917eb3b2d9e20d
Liem Nguyen <nguyenthanhliem1708@gmail.com>
2022-12-17 09:26:49 +0700

    LiemNT - add uni's info to news mapping

--- Qick/Services/AutoMapper/NewsProfile.cs
+++ Qick/Services/AutoMapper/NewsProfile.cs
@@ -9,7 +9,9 @@ namespace Qick.Services.AutoMapper
         public NewsProfile()
         {
             CreateMap<AddmissionNews, ListNewsResponse>()
-                .ForMember(m => m.UniSpecName, n => n.MapFrom(i => i.Uni.UniversitySpecializations.Where(x => x.Id == i.UniSpecId).Select(o => o.UniSpecName).FirstOrDefault()));
+                .ForMember(m => m.UniSpecName, n => n.MapFrom(i => i.Uni.UniversitySpecializations.Where(x => x.Id == i.UniSpecId).Select(o => o.UniSpecName).FirstOrDefault()))
+                .ForMember(m => m.UniName, n => n.MapFrom(i => i.Uni.UniName))
+                .ForMember(m => m.UniCode, n => n.MapFrom(i => i.Uni.UniCode));
         }
     }
 }
